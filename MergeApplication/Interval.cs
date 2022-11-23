bf82bd6d01f058b8b4b557e221a2ce11c3cae7d7
Luis Roth <luis.roth@t-online.de>
2022-11-23 09:32:06 +0100

    Coding Task 2 Merge Application - poc

---
+++ MergeApplication/Interval.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MergeApplication;
+
+public class Interval
+{
+    private long _start;
+    private long _end;
+
+    public Interval(string interval)
+    {
+        var digits = new List<long>();
+        foreach (Match digit in Regex.Matches(interval, "-?[0-9]+"))
+            digits.Add(long.Parse(digit.Value));
+        
+        digits.Sort();
+        _start = digits[0];
+        _end = digits[1];
+    }
+
+    public bool Contains(Interval interval)
+    {
+        return interval._start >= _start && interval._start <= _end || interval._end <= _end && interval._end >= _start ||
+               interval._start <= _start && interval._end >= _end;
+    }
+
+    public void Merge(Interval interval)
+    {
+        if (interval._start < _start)
+            _start = interval._start;
+        if (interval._end > _end)
+            _end = interval._end;
+    }
+
+    public override string ToString()
+    {
+        return $"[{_start},{_end}]";
+    }
+}
--- MergeApplication/Program.cs
+++ MergeApplication/Program.cs
@@ -1,3 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text.RegularExpressions;
+using MergeApplication;
+
+if (!IsInputValid(args))
+    return;
+
+var mergedIntervals = MergeIntervals(args);
+Console.WriteLine(string.Join(" ", mergedIntervals));
+
+static List<Interval> MergeIntervals(string[] inputs)
+{
+    var mergedIntervals = new List<Interval>();
+    foreach (var interval in inputs)
+    {
+        if (mergedIntervals.Count == 0)
+        {
+            mergedIntervals.Add(new Interval(interval));
+            continue;
+        }
+
+        var intervalObject = new Interval(interval);
+        var merged = false;
+        foreach (var mergedInterval in mergedIntervals)
+        {
+            if (mergedInterval.Contains(intervalObject))
+            {
+                mergedInterval.Merge(intervalObject);
+                merged = true;
+                break;
+            }
+        }
+        if(!merged)
+            mergedIntervals.Add(intervalObject);
+    }
+
+    return mergedIntervals;
+}
+
+bool IsInputValid(string[] inputs)
+{
+    return inputs.All(x => Regex.IsMatch(x, @"\[-?[0-9]+,-?[0-9]+\]"));
+}
